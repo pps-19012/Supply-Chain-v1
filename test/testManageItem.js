b217f797c0b9af024045cea2d547a9425bedf5be
Pushpendra Pratap Singh <pushpendrasingh.19012@gmail.com>
2022-06-20 03:23:48 +0530

    Added unit test file
    Checks if result and identifier is same.

--- test/testManageItem.js
+++ test/testManageItem.js
@@ -1,5 +1,16 @@
-const ItemManager = artifacts.require("/ItemManager.sol");
+const ItemManager = artifacts.require("./ItemManager.sol");
 
 contract("ItemManager", accounts => {
-    
+    it("...should be able to add an item", async function() {
+        const itemManagerInstance = await ItemManager.deployed(); //makes instance related to truffle contract library
+        const itemName = "test1";
+        const itemPrice = 500;
+        
+        const result = await itemManagerInstance.createItem(itemName, itemPrice, {from:accounts[0]});
+        //console.log(result);
+        assert.equal(result.logs[0].args._itemIndex, 0, "It is not the first item");
+
+        const item = await itemManagerInstance.items(0);
+        assert.equal(item._identifier, itemName, "The identifier was different");
+    });
 });
